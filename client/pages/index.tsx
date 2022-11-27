02fb811ae9541f434d2e2229f4702a85639e6fa5
janupasilva2005 <janupasilva2005@gmail.com>
2022-11-28 04:45:44 +0530

    Intial code for client

---
+++ client/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from '../components/container'
+import { Header } from '../components/header'
+
+const Index = () => {
+  return (
+    <div>
+      <Container>
+        <Header title={`Welcome to my chat application`} />
+      </Container>
+    </div>
+  )
+}
+
+export default Index
