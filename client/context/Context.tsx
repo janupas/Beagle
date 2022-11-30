77c3d9cef3eec4dd5f104f495bc92f42c99fe1f4
janupasilva2005 <janupasilva2005@gmail.com>
2022-11-30 20:12:15 +0530

    Public chat functionality is done without saving messages to database

--- client/context/Context.tsx
+++ client/context/Context.tsx
@@ -1,14 +1,14 @@
 import { createContext, ReactNode, useState } from 'react'
+import socket from '../socket/socket'
 
 export const context = createContext({})
 
 export const Context = ({ children }: { children: ReactNode }) => {
   const [name, setName] = useState<string>('')
-
-  const joinPublicRoom = () => {}
+  const [id, setId] = useState<string>('')
 
   return (
-    <context.Provider value={{ name, setName, joinPublicRoom }}>
+    <context.Provider value={{ name, setName, id, setId }}>
       {children}
     </context.Provider>
   )
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,11 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { Container } from '../components/container'
 import { Header } from '../components/header'
 import { Input } from '../components/input'
 import { Button } from '../components/button'
 import { context } from '../context/Context'
 import { useRouter } from 'next/router'
+import socket from '../socket/socket'
 
 const Index = () => {
   const router = useRouter()
@@ -13,7 +14,8 @@ const Index = () => {
   const handleSubmit = (e: any) => {
     e.preventDefault()
 
-    if (name.length < 5 || name.length >= 25) {
+    // Validating the form
+    if (name.length < 2 || name.length >= 25) {
       alert(
         'Please insert a name more than 5 characters and less than 25 characters'
       )
