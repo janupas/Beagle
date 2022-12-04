cace1fd4fcdb7d9948286a0a7bd385b74faf2a24
janupasilva2005 <janupasilva2005@gmail.com>
2022-12-04 10:58:55 +0530

    Added types for the container

--- client/components/container/index.tsx
+++ client/components/container/index.tsx
@@ -1,10 +1,38 @@
 import Styles from './container.module.scss'
 import { ReactNode } from 'react'
 
-interface ContainerProps {
-  children: ReactNode | ReactNode[]
+export enum ContainerTypes {
+  DEFAULT = 'default',
+  CHAT = 'chat',
 }
 
-export const Container = ({ children }: ContainerProps) => {
-  return <div className={Styles.container}>{children}</div>
+interface ContainerProps {
+  children: ReactNode | ReactNode[]
+  type?: ContainerTypes
+}
+
+export const Container = ({ children, type }: ContainerProps) => {
+  let inlineStyles: Object = {}
+
+  if (typeof type === 'undefined') {
+    type = ContainerTypes.DEFAULT
+  }
+
+  // Giving a width 0f 95% percent to the chat container
+  switch (type) {
+    case ContainerTypes.DEFAULT:
+      inlineStyles = { width: '80%' }
+      break
+    case ContainerTypes.CHAT:
+      inlineStyles = { width: '90%', maxWidth: '1000px' }
+      break
+    default:
+      break
+  }
+
+  return (
+    <div className={`${Styles.container}`} style={inlineStyles}>
+      {children}
+    </div>
+  )
 }
