99db64a8c63fb69c4fb0db781a3f6db0e249e2f0
janupasilva2005 <janupasilva2005@gmail.com>
2022-12-04 07:56:35 +0530

    Added types for the socket object

--- client/socket/socket.ts
+++ client/socket/socket.ts
@@ -1,10 +1,8 @@
-import { io } from 'socket.io-client'
+import { io, Socket } from 'socket.io-client'
 
-const socket = io(`${process.env.NEXT_PUBLIC_SERVER_URL}`)
+export const socket: Socket = io(`${process.env.NEXT_PUBLIC_SERVER_URL}`)
 
 // Set the socket id to local storage
 socket.on('connect', () => {
   console.log('Socket connected: ' + socket.id)
 })
-
-export default socket
